2c0a4c77a0dc4cb2567c48961d3cad85816d484e
Jack Applegame <japplegame@gmail.com>
2020-05-23 21:23:30 +0300

    Benchmark changes.

--- bench/run.exs
+++ bench/run.exs
@@ -1,14 +1,18 @@
 defmodule Bench do
+  def reset_rand(_), do: :rand.seed(:exrop, {1, 2, 3})
+
   def run(size) do
     IO.puts("============ #{size} elements ============\n")
     :rand.seed(:exrop, {1, 2, 3})
     data = Enum.shuffle(1..size)
     set = Enum.reduce(data, MapSet.new(), fn x, set -> MapSet.put(set, x) end)
     tree = Enum.reduce(data, AVLTree.new(), fn x, tree -> AVLTree.put(tree, x) end)
+    gb_set = Enum.reduce(data, :gb_sets.new(), fn x, set -> :gb_sets.add_element(x, set) end)
 
     IO.puts("Flat size (bytes):")
     IO.puts("MapSet:  #{:erts_debug.flat_size(set) - size}")
     IO.puts("AVLTree: #{:erts_debug.flat_size(tree) - size}")
+    IO.puts("gb_set: #{:erts_debug.flat_size(gb_set) - size}")
 
     gen_element = fn _ -> :rand.uniform(size * 2) end
 
@@ -17,8 +21,11 @@ defmodule Bench do
     Benchee.run(
       %{
         "MapSet SEARCH" => fn x -> MapSet.member?(set, x) end,
-        "AVLTree SEARCH" => fn x -> AVLTree.member?(tree, x) end
+        "AVLTree SEARCH" => fn x -> AVLTree.member?(tree, x) end,
+        "gb_set SEARCH" => fn x -> :gb_sets.is_member(x, gb_set) end
       },
+      warmup: 2,
+      before_scenario: &reset_rand/1,
       before_each: gen_element,
       time: 5,
       print: %{
@@ -32,8 +39,11 @@ defmodule Bench do
     Benchee.run(
       %{
         "MapSet INSERT" => fn x -> MapSet.put(set, x) end,
-        "AVLTree INSERT" => fn x -> AVLTree.put(tree, x) end
+        "AVLTree INSERT" => fn x -> AVLTree.put(tree, x) end,
+        "gb_set INSERT" => fn x -> :gb_sets.add_element(x, gb_set) end
       },
+      warmup: 2,
+      before_scenario: &reset_rand/1,
       before_each: gen_element,
       time: 5,
       print: %{
@@ -47,8 +57,11 @@ defmodule Bench do
     Benchee.run(
       %{
         "MapSet DELETE" => fn x -> MapSet.delete(set, x) end,
-        "AVLTree DELETE" => fn x -> AVLTree.delete(tree, x) end
+        "AVLTree DELETE" => fn x -> AVLTree.delete(tree, x) end,
+        "gb_set DELETE" => fn x -> :gb_sets.delete_any(x, gb_set) end
       },
+      warmup: 2,
+      before_scenario: &reset_rand/1,
       before_each: gen_element,
       time: 5,
       print: %{
